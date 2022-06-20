30b93d40d33b0ec3bd990b43a636c01eb2071509
Skilloff <offskilloffical@gmail.com>
2022-06-20 22:59:14 +0300

    init;

---
+++ TRN-CSGO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TRN_CSGO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
